7b020f4ac8183b556fb14395258de5aa55b655cb
Haabeel <khalfayhaabeel@gmail.com>
2024-02-18 10:02:09 +0400

    Initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      lightPrimary: "#EEF0E5",
+      lightSecondary: "#163020",
+      lightAccent: "#B6C4B6",
+      lightDarkCommon: "#304D30",
+      darkPrimary: "#26272C",
+      darkSecondary: "#4E7E52",
+      darkAccent: "#FCEFF9",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
